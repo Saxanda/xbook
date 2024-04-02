47e2c2c6fef451abc3a31de563cecd3c817e1557
Ihor Dzhevalo <125574071+Dzheva@users.noreply.github.com>
2024-04-02 22:08:32 +0300

    folder xbook-api (#1)
    * folder xbook-api
    
    * Delete .idea directory

---
+++ xbook-api/src/main/java/com/xbook/xbook/XbookApiApplication.java
@@ -0,0 +1,13 @@
+package com.xbook.xbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XbookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XbookApiApplication.class, args);
+	}
+
+}
---
+++ xbook-api/src/test/java/com/xbook/xbook/XbookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xbook.xbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XbookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
