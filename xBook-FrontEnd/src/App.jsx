428993ced9651ee2265b8af1f471b8193dfaf9e6
DmytroBrazhnyk <139158565+DmytroBrazhnyk@users.noreply.github.com>
2024-04-04 19:56:09 +0300

    add components and redact file to scss

--- xBook-FrontEnd/src/App.jsx
+++ xBook-FrontEnd/src/App.jsx
@@ -1,12 +1,14 @@
 
-import './App.css'
+import './App.scss'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
 
 function App() {
 
   return (
     <Router>
+      <Header></Header>
       <Routes>
         <Route></Route>
         <Route></Route>
