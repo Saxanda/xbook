aba58974033c774f9db3df1fa3b74847ddc98fd2
Ihor Dzheva <idzheva@gmail.com>
2024-04-08 11:33:51 +0300

    checkstyle and sonar added

--- xbook-api/src/main/java/com/xbook/xbook/XbookApiApplication.java
+++ xbook-api/src/main/java/com/xbook/xbook/XbookApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class XbookApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(XbookApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(XbookApiApplication.class, args);
+    }
 }
