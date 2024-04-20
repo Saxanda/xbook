fa52e4c1910a17fa1dfecaacdd1dee26a1adc8e2
Ruslan <saxanda@gmail;.com>
2024-04-20 21:34:40 +0200

    [U35]  corsConfigurationSource

--- xbook-api/src/main/java/app/controller/UserController.java
+++ xbook-api/src/main/java/app/controller/UserController.java
@@ -8,6 +8,7 @@ import app.entity.User;
 import app.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -22,6 +23,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("api/v1/users")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173", maxAge = 3600)
 public class UserController {
     private final UserService userService;
     private final UserMapper userMapper;
