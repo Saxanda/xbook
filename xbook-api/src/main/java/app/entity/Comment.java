5ad5897b8697a8dbb01c7852b21e764443b3e36c
Ruslan <saxanda@gmail;.com>
2024-04-21 15:52:40 +0200

    [US19-20] Created Post with endpoints

--- xbook-api/src/main/java/app/entity/Comment.java
+++ xbook-api/src/main/java/app/entity/Comment.java
@@ -1,5 +1,6 @@
 package app.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
@@ -14,14 +15,15 @@ import lombok.Data;
 @Table(name = "comments")
 public class Comment extends AbstractEntity {
 
-    // Other fields and mappings...
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id")
     private User user;
+
     @Lob
     @Column(name = "content", columnDefinition = "CLOB")
     private String content;
 
-
+    @Column(name = "post_id")
+    private Long postID;
 
 }
