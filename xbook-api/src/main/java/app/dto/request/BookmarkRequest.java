f4947bcc482eccc5b6bd1e5bf76f5c32cea72ad6
Ruslan <saxanda@gmail;.com>
2024-04-28 20:03:14 +0200

    [US24] Initial commit Bookmark endpoint

---
+++ xbook-api/src/main/java/app/dto/request/BookmarkRequest.java
@@ -0,0 +1,4 @@
+package app.dto.request;
+
+public class BookmarkRequest {
+}
---
+++ xbook-api/src/main/java/app/service/BookmarkService.java
@@ -0,0 +1,24 @@
+package app.service;
+
+import app.dto.request.BookmarkRequest;
+import app.dto.response.BookmarkResponse;
+
+import java.util.List;
+
+public class BookmarkService {
+    public BookmarkResponse createBookmark(BookmarkRequest bookmarkRequest) {
+        return null;
+    }
+
+    public List<BookmarkResponse> getAllBookmarksByUserId(Long userId) {
+        return null;
+    }
+
+    public List<BookmarkResponse> getAllBookmarksByPostId(Long postId) {
+        return null;
+    }
+
+    public boolean deleteBookmark(Long bookmarkId) {
+        return false;
+    }
+}
