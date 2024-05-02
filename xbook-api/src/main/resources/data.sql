33b4382621c3ad17a82363a8d7286e2d7d478a57
Vladyslav Soruk <sorukv@gmail.com>
2024-05-02 18:56:01 +0300

    Create endpoint for the forgot password page with the mail notification, field `isEdited` added in messages

--- xbook-api/src/main/resources/data.sql
+++ xbook-api/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO users (name, surname, email, password, date_of_birth, gender, role, photo, avatar, address, is_activated)
-VALUES ('John', 'Doe', 'sorukv@gmail.com', '$2a$10$iwRji5mPlwatlHuKkof20Ot/pQfuUIqrpEviIRk4gMGdeH1XWaavm', '1990-01-01', 'male', 'USER', 'photo.jpg', 'avatar.jpg', '123 Main Street', true);
+VALUES ('John', 'Doe', 'john.doe@example.com', '$2a$10$iwRji5mPlwatlHuKkof20Ot/pQfuUIqrpEviIRk4gMGdeH1XWaavm', '1990-01-01', 'male', 'USER', 'photo.jpg', 'avatar.jpg', '123 Main Street', true);
 
 INSERT INTO users (name, surname, email, password, date_of_birth, gender, role, photo, avatar, address, is_activated)
 VALUES ('Alice', 'Smith', 'alice.smith@example.com', '$2a$10$7nnuayAu1EP9DnxjSYzwYuqtN4D8rcO0NSsvnS8jxPLDLgw6wysxG', '1985-05-15', 'female', 'USER', 'alice.jpg', 'avatar.jpg', '456 Elm Street', true);
