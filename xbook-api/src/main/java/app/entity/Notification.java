487d391acec60e2d77a5ddf43c391a97631c1011
Ruslan <saxanda@gmail;.com>
2024-05-02 22:35:28 +0200

    [US24] Notification entity is first commit

---
+++ xbook-api/src/main/java/app/entity/Notification.java
@@ -0,0 +1,30 @@
+package app.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "notifications")
+public class Notification extends AbstractEntity{
+
+    @Column(nullable = false)
+    private String message;
+
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recipient_id", nullable = false)
+    private User recipient;
+
+    @Column(nullable = false)
+    private boolean readStatus;
+}
