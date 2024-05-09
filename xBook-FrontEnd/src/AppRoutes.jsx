25ce83eb6a52d8b2a32ab07f6f9f6434ce722ace
Pokhyl Evhenii <genyapohil19@gmail.com>
2024-05-09 16:53:22 +0300

    changed App.jsx

--- xBook-FrontEnd/src/AppRoutes.jsx
+++ xBook-FrontEnd/src/AppRoutes.jsx
@@ -4,12 +4,16 @@ import PrivateRoutes from "./helpers/PrivateRoutes";
 import LoginPage from "./Pages/LoginPage";
 import Home from "./Pages/Home";
 import Bookmarks from "./Pages/Bookmarks";
+import UpdatePasswordPage from './Pages/UpdatePasswordPage';
+import ForgotPage from './Pages/ForgotPage';
 
 export default function AppRoutes() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/forgot-page" element={<ForgotPage />} />
+          <Route path="/forgot-password/" element={<UpdatePasswordPage />} />
         <Route element={<PrivateRoutes />}>
           <Route path="/" element={<Home />} />
           <Route path="/bookmarks" element={<Bookmarks />} />
