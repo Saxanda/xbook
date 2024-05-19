f17f00f36eecc6cbff846dca40999a1710f010f2
Ruslan <saxanda@gmail;.com>
2024-05-19 12:42:57 +0200

    [US48] Adjust Comment response initial commit

--- xbook-api/src/main/java/app/dto/response/CommentResponse.java
+++ xbook-api/src/main/java/app/dto/response/CommentResponse.java
@@ -2,10 +2,13 @@ package app.dto.response;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class CommentResponse {
     private Long id;
     private String content;
-    private Long userId;
+    private LocalDateTime localDateTime;
+    private UserDetailsResponse author; // User's id which created comment under the post
     private Long postId;
 }
