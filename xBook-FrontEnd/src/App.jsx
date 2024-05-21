8b00966efe399a294eb5f60c1698e1570807f333
Amir Djagatspanian <overtynover96@gmail.com>
2024-05-21 21:43:34 +0300

    add changes

--- xBook-FrontEnd/src/App.jsx
+++ xBook-FrontEnd/src/App.jsx
@@ -1,27 +1,16 @@
 import './App.scss'
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header/Header';
-import ProfilePage from './Pages/ProfilePage/ProfilePage';
-import ProfilePagePosts from './components/ProfilePagePosts/ProfilePagePosts';
-import ProfilePageFriends from './components/ProfilePageFriends/ProfilePageFriends';
-import ProfileFriendRequests from './components/ProfileFriendRequests/ProfileFriendRequests';
+import AppRoutes from './AppRoutes';
 
 function App() {
-
+  
   return (
-    <Router>
-      <Header></Header>
-      <Routes>
-        <Route exact path='/profile/:id/*' element={<ProfilePage />}>
-            <Route path='' element={<ProfilePagePosts />} />
-            <Route path='friends' element={<ProfilePageFriends />} />
-            <Route path='requests' element={<ProfileFriendRequests />} />
-        </Route>
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <AppRoutes/>
+    </Provider>
   )
 }
 
 export default App
- 
