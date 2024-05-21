95a2e0dccb69c303a3e8caae67f982660ef606b2
Anna Levchenko <anna.levchenko.03@gmail.com>
2024-05-21 17:45:07 +0300

    added changes

--- xBook-FrontEnd/src/pages/Home.jsx
+++ xBook-FrontEnd/src/pages/Home.jsx
@@ -1,24 +1,25 @@
-import { useState } from 'react';
-import { Grid } from '@mui/material';
-import PostsFeed from '../components/Post/PostsFeed'
-import CreatePost from '../components/Post/CreatePost';
-
-
+import { useState } from "react";
+import { Grid } from "@mui/material";
+import PostsFeed from "../components/Post/PostsFeed";
+import CreatePost from "../components/Post/CreatePost";
 
 export default function Home() {
-    const [refresh, setRefresh] = useState(false);
+  const [refresh, setRefresh] = useState(false);
 
-    const handlePostCreated = () => {
-        setRefresh(prev => !prev); 
-    };
-    return (
-        <section  className='home_posts_container'>
-            <Grid item xs={12} sm={8} md={6} className='create_post'>
-                <CreatePost onPostCreated={handlePostCreated} ></CreatePost>
-            </Grid>
-            <Grid item xs={12} sm={8} md={6}>
-                <PostsFeed refresh={refresh} handlePostCreated={handlePostCreated} ></PostsFeed>
-            </Grid>
-        </section>
-    );
+  const handlePostCreated = () => {
+    setRefresh((prev) => !prev);
+  };
+  return (
+    <section className="home_posts_container">
+      <Grid item xs={12} sm={8} md={6} className="create_post">
+        <CreatePost onPostCreated={handlePostCreated}></CreatePost>
+      </Grid>
+      <Grid item xs={12} sm={8} md={6}>
+        <PostsFeed
+          refresh={refresh}
+          handlePostCreated={handlePostCreated}
+        ></PostsFeed>
+      </Grid>
+    </section>
+  );
 }
