3b3088d9f30ae4327e49566c8619a88adafc5ae2
Ruslan <saxanda@gmail;.com>
2024-05-28 23:13:34 +0200

    [US50b] method to delete post and associated with likes, comments and notifications

--- xbook-api/src/main/java/app/repository/CommentRepository.java
+++ xbook-api/src/main/java/app/repository/CommentRepository.java
@@ -6,7 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     Page<Comment> findByPostId(Long postId, Pageable pageable);
+
+    List<Comment> findAllByPostId(Long postId);
 }
