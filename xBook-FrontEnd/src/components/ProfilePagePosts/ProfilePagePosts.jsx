0e5ed9ab9459ac3ed97f6ccadcfb254bd52569fc
AmirDjag <165702325+AmirDjag@users.noreply.github.com>
2024-06-05 15:46:16 +0300

    US-56 (#89)
    * add
    
    * add posts
    
    * add changes
    
    * add creat post to profile
    
    * fix bugs
    
    * add changes

--- xBook-FrontEnd/src/components/ProfilePagePosts/ProfilePagePosts.jsx
+++ xBook-FrontEnd/src/components/ProfilePagePosts/ProfilePagePosts.jsx
@@ -1,107 +1,99 @@
-import './ProfilePagePosts.scss'
+import './ProfilePagePosts.scss';
 import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
 import CottageIcon from '@mui/icons-material/Cottage';
 import CakeIcon from '@mui/icons-material/Cake';
 import Button from '@mui/material/Button';
 import EmailIcon from '@mui/icons-material/Email';
 import MaleIcon from '@mui/icons-material/Male';
 import FemaleIcon from '@mui/icons-material/Female';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import TransgenderIcon from '@mui/icons-material/Transgender';
+import { useSelector, useDispatch } from 'react-redux';
 import { modalEditProfile } from '../../redux/profile/profileSlice';
+import ProfilePostsList from '../ProfilePostsList/ProfilePostsList';
+import CreatePost from '../Post/CreatePost';
+import { useState } from 'react';
 
-export default function ProfilePagePosts(){
-    const dispatch = useDispatch()
-    let urlID = useParams().id;
-    urlID = parseInt(urlID);
-    const {obj} = useSelector(state => state.profile.profileData)   
-    
+export default function ProfilePagePosts() {
+    const { obj } = useSelector(state => state.profile.profileData);
+    const dispatch = useDispatch();
+
     const modalEditProfileOpen = () => {
         dispatch(modalEditProfile(true));
     };
 
-    return(
+    const [refresh, setRefresh] = useState(false);
+
+    const handlePostCreated = () => {
+        setRefresh(prev => !prev);
+    };
+
+    return (
         <>
             <div className='profilePosts'>
                 <div className='profilePosts__info'>
-                    <div className='userInfo'>
-                        <Typography variant='h6' sx={{fontWeight: 600}}>About</Typography>
-                        <ul className='userInfo__list'>
-                            {
-                                obj.address ?
-                            <li className='userInfo__item'>
-                                <CottageIcon color='action' sx={{marginRight: "12px"}}/>
-                                <Typography variant='subtitle2'>
-                                    Live in <span>{obj.address}</span>
-                                </Typography>
-                            </li>
-                            :
-                            null
-                            }
-                            {
-                                obj.dob ?
-                            <li className='userInfo__item'>
-                                <CakeIcon color='action' sx={{marginRight: "12px"}}/>
-                                <Typography variant='subtitle2'>
-                                    {obj.dob}
-                                </Typography>
-                            </li>
-                            :
-                            null
-                            }
-                            {
-                                obj.email ?
-                            <li className='userInfo__item'>
-                                <EmailIcon color='action'  sx={{marginRight: "12px"}}/>
-                                <Typography variant='subtitle2'>
-                                    {obj.email}
-                                </Typography>
-                            </li>
-                            : 
-                            null
-                            }
-
-                            {
-                                obj.gender ?
-                            <li className='userInfo__item'>
-                                {obj.gender === "men" || obj.gender === "male"? 
-                                    <MaleIcon color='action'  sx={{marginRight: "12px"}}/>
-                                    :
-                                    <FemaleIcon color='action'  sx={{marginRight: "12px"}}/>
+                    <Paper elevation={3} sx={{ borderRadius: "8px", marginTop: "20px" }}>
+                        <div className='userInfo'>
+                            <Typography variant='h6' sx={{ fontWeight: 600 }}>About</Typography>
+                            <ul className='userInfo__list'>
+                                {obj.address &&
+                                    <li className='userInfo__item'>
+                                        <CottageIcon color='action' sx={{ marginRight: "12px" }} />
+                                        <Typography variant='subtitle2'>
+                                            Live in <span>{obj.address}</span>
+                                        </Typography>
+                                    </li>
                                 }
-                                <Typography variant='subtitle2'>
-                                    {obj.gender}
-                                </Typography>
-                            </li>
-                            : 
-                            null
+                                {obj.dob &&
+                                    <li className='userInfo__item'>
+                                        <CakeIcon color='action' sx={{ marginRight: "12px" }} />
+                                        <Typography variant='subtitle2'>
+                                            {obj.dob}
+                                        </Typography>
+                                    </li>
+                                }
+                                {obj.email &&
+                                    <li className='userInfo__item'>
+                                        <EmailIcon color='action' sx={{ marginRight: "12px" }} />
+                                        <Typography variant='subtitle2'>
+                                            {obj.email}
+                                        </Typography>
+                                    </li>
+                                }
+                                {obj.gender &&
+                                    <li className='userInfo__item'>
+                                        {obj.gender === "man" || obj.gender === "male" ?
+                                            <MaleIcon color='action' sx={{ marginRight: "12px" }} />
+                                            : obj.gender === "woman" || obj.gender === "female" ?
+                                                <FemaleIcon color='action' sx={{ marginRight: "12px" }} />
+                                                :
+                                                <TransgenderIcon color='action' sx={{ marginRight: "12px" }} />
+                                        }
+                                        <Typography variant='subtitle2'>
+                                            {obj.gender}
+                                        </Typography>
+                                    </li>
+                                }
+                            </ul>
+                            {obj.user === "myUser" &&
+                                <Button
+                                    variant="contained"
+                                    color='info'
+                                    onClick={modalEditProfileOpen}
+                                    sx={{
+                                        width: "100%",
+                                        marginTop: "16px"
+                                    }}
+                                >Edit Bio</Button>
                             }
-                        </ul>
-                        {
-                            obj.user === "myUser" ?
-                            <Button 
-                            variant="contained"
-                            color='info'
-                            onClick={modalEditProfileOpen} 
-                            sx={{
-                                width: "100%", 
-                                marginTop: "16px"
-                            }}
-                            >Edit Bio</Button>
-                            :
-                            null
-                        }
-                    </div>
+                        </div>
+                    </Paper>
                 </div>
-                <div>
-                    <div style={{maxWidth: "500px", width: "300px", height: "128px", backgroundColor: "grey", marginBottom: "16px"}}>
-                    </div>
-                    <div style={{maxWidth: "500px", width: "300px", height: "93px", backgroundColor: "grey", marginBottom: "16px"}}>
-                    </div>
-                    <div style={{maxWidth: "500px", width: "300px", height: "500px", backgroundColor: "grey"}}>
-                    </div>
+                <div style={{ display: 'flex', flexDirection: "column" }}>
+                    {obj.user === "myUser" ? <CreatePost onPostCreated={handlePostCreated} style={{ marginTop: 0 }} /> : null}
+                    <ProfilePostsList refresh={refresh} handlePostCreated={handlePostCreated} />
                 </div>
             </div>
         </>
-    )
+    );
 }
---
+++ xBook-FrontEnd/src/components/ProfilePostsList/ProfilePostsList.jsx
@@ -0,0 +1,123 @@
+import './ProfilePostsList.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { Typography, Box } from '@mui/material';
+import ProfilePost from '../ProfilePost/ProfilePost';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import CircularProgress from '@mui/material/CircularProgress';
+import { getUserPosts } from '../../redux/profile/profileSlice';
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default function ProfilePostsLists({ refresh, handlePostCreated }) {
+
+    const dispatch = useDispatch();
+    let urlID = useParams().id;
+    urlID = parseInt(urlID);
+
+    const [postsData, setPostsData] = useState([]);
+    const [page, setPage] = useState(0);
+    const [hasMore, setHasMore] = useState(true);
+
+    const totalPosts = useSelector(state => state.profile.userPosts.obj?.totalElements);
+    const userId = useSelector(state => state.profile.profileData.obj.id)
+
+    useEffect(() => {
+        const fetchInitialPosts = async () => {
+            try {
+                const response = await dispatch(getUserPosts({ userId: urlID, page: 0 }));
+                const data = response.payload;
+                setPostsData(data.content);
+                setPage(1);
+                setHasMore(!data.last);
+            } catch (error) {
+                console.error('Error fetching posts:', error);
+            }
+        };
+
+        fetchInitialPosts();
+    }, [urlID, refresh]);
+
+    const handlePagination = async () => {
+        if (!hasMore) return;
+        try {
+            const response = await dispatch(getUserPosts({ userId: urlID, page: page }));
+            const data = response.payload;
+            setPostsData(prevPosts => [...prevPosts, ...data.content]);
+            setPage(prevPage => prevPage + 1);
+            setHasMore(!data.last);
+        } catch (error) {
+            console.error('Error fetching posts:', error);
+        }
+    };
+
+    const getAuthToken = () => {
+        return sessionStorage.getItem('token') || localStorage.getItem('token');
+    };
+
+    const addToBookmarks = async (postId) => {
+        try {
+            const token = getAuthToken();
+            const response = await axios.post(`http://localhost:8080/api/v1/bookmarks`, {
+                postId: postId, userId
+            }, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            console.log('Post added to bookmarks:', response.data);
+        } catch (error) {
+            console.error('Error adding post to bookmarks:', error);
+        }
+    };
+
+    const removeFromBookmarks = async (postId) => {
+        try {
+            const token = getAuthToken();
+            const { data: bookmarksData } = await axios.get(`http://localhost:8080/api/v1/bookmarks/post/${postId}`, {
+                           headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            const bookmarkData = bookmarksData.content[0];
+            const response = await axios.delete(`http://localhost:8080/api/v1/bookmarks/${bookmarkData.bookmarkId}`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            console.log('Post removed from bookmarks:', response.data);
+        } catch (error) {
+            console.error('Error deleting a post from bookmarks:', error);
+        }
+    };
+
+    return (
+        <>
+            {postsData.length > 0 ? 
+                <InfiniteScroll
+                    style={{marginTop: "20px"}}
+                    dataLength={postsData.length}
+                    next={handlePagination}
+                    hasMore={hasMore}
+                    loader={
+                        <div style={{display: "flex", alignItems: "center", justifyContent: "center", padding: "10px 0 10px 0"}}>
+                            <CircularProgress />
+                        </div>
+                    }
+                >
+                    <ul className='postsList'>
+                        {postsData.map(post => (
+                            <li key={post.id}>
+                                <ProfilePost post={post} refresh={handlePostCreated} addToBookmarks={addToBookmarks} removeFromBookmarks={removeFromBookmarks} />
+                            </li>
+                        ))}
+                    </ul>
+                </InfiniteScroll>
+                :
+                <Typography variant='h5' sx={{my: "100px", alignSelf: "center"}}>
+                    <Box sx={{color: "text.secondary", textTransform: "uppercase"}}>NO POSTS</Box>
+                </Typography>
+            }
+        </>
+    );
+}
