6115e056c90ea83a5f98ab04d0283a8868a07fbd
SerheI Pichaievskiy <166049798+seyogaxd@users.noreply.github.com>
2024-06-09 21:45:19 +0200

    Us 55 (#103)
    * Changed chats
    
    Changed chats to url
    
    * Added stomp
    
    Trying to add websockets to chats
    
    * websocket connected
    
    Connected to WebSockets
    
    * Repaired websockets
    
    Repaired websockets
    
    * Fixed websockets
    
    Fixed websockets
    
    * Changed Chats
    
    Changed chats visual
    
    * deleted node
    
    deleted node from xbook
    
    * deleted package.json
    
    deleted package.json

--- xBook-FrontEnd/src/pages/ChatPage/ChatPage.jsx
+++ xBook-FrontEnd/src/pages/ChatPage/ChatPage.jsx
@@ -1,190 +1,283 @@
-import './Styles/ChatPageStyle.scss';
-
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
+import "./Styles/ChatPageStyle.scss";
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
 import Users from "../../components/Chat/Users/Users";
-import Window from '../../components/Chat/Window/Window';
-
-import testImage from '../../../public/image/send_image_black.png';
-import { useDispatch, useSelector } from 'react-redux';
+import Window from "../../components/Chat/Window/Window";
+import { useParams } from "react-router-dom";
+import { Client } from "@stomp/stompjs";
+import { Box, Grid, TextField, IconButton } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
 
 export default function ChatPage() {
-    const [users, setUsers] = useState([]); 
-    const [loading, setLoading] = useState(true);
-    const [inputText, setInputText] = useState('');
-    const [isRedact, setIsRedact] = useState(false);
-    const [messageId, setMessageId] = useState(0);
-    const [id, setID] = useState(() => {
-        const savedChatID = localStorage.getItem('lastActiveChatID');
-        return savedChatID !== null ? parseInt(savedChatID) : 2;
-    });
-    const [firstUserId, setFirstUserId] = useState((0));
-    const [messages, setMessages] = useState([]);
-    const [trigger, setTrigger] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [inputText, setInputText] = useState("");
+  const [isRedact, setIsRedact] = useState(false);
+  const [messageId, setMessageId] = useState(0);
+  const [id, setID] = useState(() => {
+    const savedChatID = localStorage.getItem("lastActiveChatID");
+    return savedChatID !== null ? parseInt(savedChatID) : 2;
+  });
+  const [userEmail, setUserEmail] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [trigger, setTrigger] = useState(false);
+  const [chatClear, setChatClear] = useState(false);
+  const [deleteTrigger, setDeleteTrigger] = useState(false);
+  const [token, setToken] = useState(
+    localStorage.getItem("token") || sessionStorage.getItem("token")
+  );
+  const [someState, setSomeState] = useState("");
+  let urlID = useParams().id;
+  urlID = parseInt(urlID);
 
-    const [chatClear, setChatClear] = useState(false);
+  const stompClientRef = useRef(null);
 
-    const [deleteTrigger, setDeleteTrigger] = useState(false);
-    const dispatch = useDispatch();
-
-    const [token, setToken] = useState(localStorage.getItem("token") || sessionStorage.getItem("token"));
-    
-    useEffect(() => { // Загрузка чата
-        const fetchMessages = async () => {
-            //console.log("success" + id)
-            if(users.length > 0 && id!=-1)
-                {
-                    try {
-                const response = await axios.get(`http://localhost:8080/api/chats/messages/${id}?page=0&size=20`, {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                });
-                setMessages(response.data.content);
-                console.log(response.data.content)
-                setLoading(false);
-                setChatClear(false);
-            } catch (error) {
-                console.error('Error fetching messages:', error);
-                setChatClear(true);
+  useEffect(() => {
+    const fetchMessages = async () => {
+      if (users.length > 0 && id !== -1) {
+        try {
+          const response = await axios.get(
+            `http://localhost:8080/api/v1/chats/messages/${urlID}?page=0&size=20`,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
             }
-                }
-                else{
-                    setChatClear(true);
-                }
-            
-        };
-
-        fetchMessages();
-    }, [token, id, trigger, deleteTrigger, users]);
-
-    const handleIdChange = (index) => { //saving the chat selected by the user
-        setID(index);
-        localStorage.setItem('lastActiveChatID', index); 
+          );
+          setMessages(response.data.content);
+          setLoading(false);
+          setChatClear(false);
+        } catch (error) {
+          console.error("Error fetching messages:", error);
+          setChatClear(true);
+        }
+      } else {
+        setChatClear(true);
+      }
     };
-    const changeUserArray = (users) => {
-        setUsers(users);
+
+    fetchMessages();
+  }, [token, id, trigger, deleteTrigger, users, someState]);
+
+  const handleIdChange = (index) => {
+    setID(index);
+    localStorage.setItem("lastActiveChatID", index);
+
+    // Subscribe to the new chat
+    if (stompClientRef.current && userEmail) {
+      const subscriptionPath = `/user/${userEmail}/queue/message-status`;
+      stompClientRef.current.subscribe(subscriptionPath, (message) => {
+        console.log("Subscription received:", message.body);
+      });
+
+      // Send request to update message status
+      stompClientRef.current.publish({
+        destination: `/app/update-message-status/${0}`,
+        body: JSON.stringify('READ'),
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log("yes");
     }
-    const handleMessageSend = async () => { // Sending and editing a message
-        if(isRedact) //editin
-        {
-            if (inputText !== "") {
-                try {
-                    const response = await axios.post(
-                        `http://localhost:8080/api/messages/update/${messageId}`,
-                        {
-                            content: inputText,
-                        },
-                        {
-                            headers: {
-                                Authorization: `Bearer ${token}`,
-                            },
-                        }
-                    );
-                    
-                    deleteTriggerChange();
-                    setInputText('');
-                } catch (error) {
-                    console.error('Error sending message:', error);
-                }
-            }
-            setIsRedact(false)
+  };
+
+  useEffect(() => {
+    const connectWebSocket = () => {
+      const stompClient = new Client({
+        brokerURL: "ws://localhost:8080/websocket",
+        connectHeaders: {
+          Authorization: `Bearer ${token}`,
+        },
+        reconnectDelay: 5000,
+        heartbeatIncoming: 4000,
+        heartbeatOutgoing: 4000,
+      });
+
+      stompClient.onConnect = (frame) => {
+        console.log("Connected to WebSocket server:", frame);
+        if (userEmail) {
+          const subscriptionPath = `/user/${userEmail}/queue/messages`;
+          console.log("Subscribing to:", subscriptionPath);
+          stompClient.subscribe(subscriptionPath, (message) => {
+            console.log("Subscription received:", message.body);
+            setSomeState(message);
+          });
+        } else {
+          console.warn("User email is not set, cannot subscribe.");
         }
-        else // sending
-        {
-            if (inputText !== "") {
-            try {
-                const response = await axios.post(
-                    'http://localhost:8080/api/messages/send',
-                    {
-                        chatId: id,
-                        contentType: 'text',
-                        content: inputText,
-                    },
-                    {
-                        headers: {
-                            Authorization: `Bearer ${token}`,
-                        },
-                    }
-                );
-                triggerChange();
-                setInputText('');
-                localStorage.setItem('lastActiveUser', 0);
-                localStorage.setItem('lastActiveUserId', 0);
-            } catch (error) {
-                console.error('Error sending message:', error);
-            }
-        }
-        }
+      };
+
+      stompClient.onStompError = (frame) => {
+        console.error("Broker reported error:", frame.headers["message"]);
+        console.error("Additional details:", frame.body);
+      };
+
+      stompClient.onWebSocketClose = (frame) => {
+        console.log("WebSocket connection closed:", frame);
+      };
+
+      stompClient.onWebSocketError = (error) => {
+        console.error("WebSocket error:", error);
+      };
+
+      stompClient.activate();
+
+      stompClientRef.current = stompClient;
     };
 
-    const changeMessage = (id, text) => { // Enable editing
-        setInputText(text);
-        setIsRedact(true);
-        setMessageId(id);
+    connectWebSocket();
+
+    return () => {
+      if (stompClientRef.current) {
+        stompClientRef.current.deactivate();
+      }
+    };
+  }, [token, userEmail]);
+
+  useEffect(() => {
+    const userEmailTaker = async () => {
+      const response = await axios.get(
+        `http://localhost:8080/api/v1/users/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setUserEmail(response.data.email);
+    };
+
+    if (id !== -1) {
+      userEmailTaker();
     }
+  }, [id, token]);
 
-    const triggerChange = () => { // trigger
-        setTrigger(prevTrigger => !prevTrigger);
-    };
-
-    const deleteTriggerChange = () => { // second trigger
-        setDeleteTrigger(prevTrigger => !prevTrigger);
-    };
-
-    const handleKeyDown = (event) => { // Sending/editing a message when pressing Enter
-        if (event.key === 'Enter') {
-            handleMessageSend();
+  const handleMessageSend = async () => {
+    if (isRedact) {
+      if (inputText !== "") {
+        try {
+          const response = await axios.post(
+            `http://localhost:8080/api/v1/messages/update/${messageId}`,
+            {
+              content: inputText,
+            },
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          deleteTriggerChange();
+          setInputText("");
+        } catch (error) {
+          console.error("Error sending message:", error);
         }
-    };
+      }
+      setIsRedact(false);
+    } else {
+      if (inputText !== "") {
+        try {
+          const headers = {
+            Authorization: `Bearer ${token}`,
+          };
+          stompClientRef.current.publish({
+            destination: "/app/chat",
+            body: JSON.stringify({
+              chatId: id,
+              contentType: "text",
+              content: inputText,
+            }),
+            headers: headers,
+          });
+          setInputText("");
+          triggerChange();
+          deleteTriggerChange();
+        } catch (error) {
+          console.error("Error sending message:", error);
+        }
+      }
+    }
+  };
 
-    const handleInputChange = (event) => {
-        setInputText(event.target.value);
-    };
+  const changeMessage = (id, text) => {
+    setInputText(text);
+    setIsRedact(true);
+    setMessageId(id);
+  };
 
-    return (
-        <div className="chat__container">
-            <ul className='chat__list'>
-                <li className="chat__users">
-                    <Users
-                        onClicked={handleIdChange}
-                        messages={messages}
-                        currentId={id}
-                        trigger={trigger}
-                        secondTrigger={deleteTrigger}
-                        triggerChange={deleteTriggerChange}
-                        changeUserArray={changeUserArray}
-                        token={token}
-                    />
-                </li>
+  const triggerChange = () => {
+    setTrigger((prevTrigger) => !prevTrigger);
+  };
 
-                {messages && (
-                    <li className="chat__window">
-                        <Window 
-                        data={messages} 
-                        token={token} 
-                        trigger={deleteTriggerChange}
-                        redactButton={changeMessage}
-                        chatClear={chatClear} 
-                        />
-                        <div className="chat__container_message-input">
-                            <input
-                                type="text"
-                                name=""
-                                id=""
-                                className='chat__message_input'
-                                value={inputText}
-                                onChange={handleInputChange}
-                                onKeyDown={handleKeyDown}
-                            />  
-                            <button onClick={handleMessageSend} className='chat__message_button'>
-                                <img src={testImage} alt="" className='chat__message_icon' />
-                            </button>
-                        </div>
-                    </li>
-                )}
-            </ul>
-        </div>
-    );
+  const deleteTriggerChange = () => {
+    setDeleteTrigger((prevTrigger) => !prevTrigger);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleMessageSend();
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  return (
+    <Box
+                className="chat__container_message-input"
+                style={{ height: "50px" }}
+              >
+      <Grid container className="chat__list">
+        <Grid item xs={12} md={3} className="chat__users">
+          <Users
+            onClicked={handleIdChange}
+            messages={messages}
+            currentId={id}
+            trigger={trigger}
+            secondTrigger={deleteTrigger}
+            triggerChange={deleteTriggerChange}
+            changeUserArray={setUsers}
+            token={token}
+            urlID={urlID}
+          />
+        </Grid>
+        <Grid item xs={12} md={9}>
+          {messages && (
+            <Box className="chat__window">
+              <Window
+                data={messages}
+                token={token}
+                trigger={deleteTriggerChange}
+                redactButton={changeMessage}
+                chatClear={chatClear}
+              />
+              <Box className="chat__container_message-input">
+                <TextField
+                  type="text"
+                  className="chat__message_input"
+                  value={inputText}
+                  onChange={handleInputChange}
+                  onKeyDown={handleKeyDown}
+                />
+                <IconButton
+                  onClick={handleMessageSend}
+                  className="chat__message_button-send"
+                  sx={{
+                    width: "50px",
+                    height: "50px",
+                    padding: 0,
+                    borderRadius: "4px",
+                  }}
+                >
+                  <SendIcon className="chat__message_icon" />
+                </IconButton>
+              </Box>
+            </Box>
+          )}
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
