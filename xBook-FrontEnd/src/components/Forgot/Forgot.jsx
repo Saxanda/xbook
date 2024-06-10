e9e7935c0ece1447a4aca776313f867ebbc9f3c7
SaIVIuray <165695218+SaIVIuray@users.noreply.github.com>
2024-06-10 13:52:51 +0300

    Us 57 (#106)
    * add US-57
    
    * improved
    
    * modified
    
    * modified
    
    * modified

--- xBook-FrontEnd/src/components/Forgot/Forgot.jsx
+++ xBook-FrontEnd/src/components/Forgot/Forgot.jsx
@@ -31,8 +31,8 @@ const ForgotPasswordForm = () => {
         <form onSubmit={handleFirstSubmit}>
           <h2 className='titleForgot'>Forgot your password</h2>
           <p className='textForgot'>To recover your password, enter your email address</p>
-          <div>
-            <input className='forgotInput'
+          <div >
+            <input className='forgotInput' 
               type="email"
               name="email"  
               value={email}
@@ -49,7 +49,7 @@ const ForgotPasswordForm = () => {
       )}
       {stage === 2 && (
         <div className='massage' >
-          <h1>A password reset link has been sent by email.</h1>
+          <h1 className='titleForgotTwo'>A password reset link has been sent by email.</h1>
           <div className='password-reset-button'>
             <button className='buttonForgot' type="button" onClick={() => window.history.back()}>Continue</button>
           </div>
