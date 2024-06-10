e2d8a76fcfb28ab4b2ec6091667791bdd1065a4d
vladyslavsoruk <125127888+vladyslavsoruk@users.noreply.github.com>
2024-06-10 16:21:33 +0300

    Small WebSocket fixes (#109)
    * Small WebSocket fixes
    
    * SpaController refinement

--- xbook-api/src/main/java/app/configuration/WebSocketConfig.java
+++ xbook-api/src/main/java/app/configuration/WebSocketConfig.java
@@ -23,7 +23,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic", "/user");
-        config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app");
         config.setUserDestinationPrefix("/user");
     }
--- xbook-api/src/main/java/app/controller/SpaController.java
+++ xbook-api/src/main/java/app/controller/SpaController.java
@@ -1,14 +1,42 @@
+//package app.controller;
+//
+//import jakarta.servlet.http.HttpServletResponse;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//import java.io.IOException;
+//
+//@Controller
+//public class SpaController {
+//    @RequestMapping(value = {
+//            "/login",
+//            "/forgot-page",
+//            "/forgot-password",
+//            "/post/**",
+//            "/bookmarks",
+//            "/chats/**",
+//            "/notifications",
+//            "/profile/**",
+//    })
+//    public String redirect(){
+//        return "forward:/index.html";
+//    }
+//
+//    @RequestMapping("/")
+//    public void login(HttpServletResponse response) throws IOException {
+//        response.sendRedirect("/login");
+//    }
+//}
+
 package app.controller;
 
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.io.IOException;
 
 @Controller
 public class SpaController {
     @RequestMapping(value = {
+            "/",
             "/login",
             "/forgot-page",
             "/forgot-password",
@@ -17,13 +45,10 @@ public class SpaController {
             "/chats/**",
             "/notifications",
             "/profile/**",
+            "/friends",
+            "/requests"
     })
     public String redirect(){
         return "forward:/index.html";
-    }
-
-    @RequestMapping("/")
-    public void login(HttpServletResponse response) throws IOException {
-        response.sendRedirect("/login");
     }
 }
