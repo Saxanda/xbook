4c638d45b2c5e4fd40e0cd47e2e5040354553c54
Ruslan Sakhanda <saxanda@gmail.com>
2024-07-11 22:11:24 +0200

    Remove unnecessary static files (#118)
    Co-authored-by: Ruslan <saxanda@gmail;.com>

--- xbook-api/src/main/java/app/controller/SpaController.java
+++ xbook-api/src/main/java/app/controller/SpaController.java
@@ -1,42 +1,15 @@
-//package app.controller;
-//
-//import jakarta.servlet.http.HttpServletResponse;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//import java.io.IOException;
-//
-//@Controller
-//public class SpaController {
-//    @RequestMapping(value = {
-//            "/login",
-//            "/forgot-page",
-//            "/forgot-password",
-//            "/post/**",
-//            "/bookmarks",
-//            "/chats/**",
-//            "/notifications",
-//            "/profile/**",
-//    })
-//    public String redirect(){
-//        return "forward:/index.html";
-//    }
-//
-//    @RequestMapping("/")
-//    public void login(HttpServletResponse response) throws IOException {
-//        response.sendRedirect("/login");
-//    }
-//}
 
 package app.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
 
 @Controller
 public class SpaController {
     @RequestMapping(value = {
-            "/",
             "/login",
             "/forgot-page",
             "/forgot-password",
@@ -45,10 +18,13 @@ public class SpaController {
             "/chats/**",
             "/notifications",
             "/profile/**",
-            "/friends",
-            "/requests"
     })
     public String redirect(){
         return "forward:/index.html";
+    }
+
+    @RequestMapping("/")
+    public void login(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/login");
     }
 }
